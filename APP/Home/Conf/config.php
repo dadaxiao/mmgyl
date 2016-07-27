780d0dff7f0c994b7508b83a315a1a0ed2fe7a0b
unknown <>
2016-07-27 22:23:57 +0800

    07/27

--- APP/Home/Conf/config.php
+++ APP/Home/Conf/config.php
@@ -8,4 +8,6 @@ return array(
 'DB_PWD'=>'1851432302',
 'DB_PORT'=>'3306',
 'DB_CHARSET'=> 'utf8',
+'DATA_CACHE_TYPE'=>'file',//设置缓存方式为file       
+'DATA_CACHE_TIME'=>'600',//缓存周期600秒 
 );
---
+++ APP/Runtime/Temp/0fea6a13c52b4d4725368f24b045ca84.php
@@ -0,0 +1,3 @@
+<?php
+//000000000120a:3:{s:3:"ret";s:2:"20";s:4:"data";a:9:{i:0;a:2:{s:8:"catename";s:6:"小吃";s:3:"cid";s:1:"1";}i:1;a:2:{s:8:"catename";s:6:"蔬菜";s:3:"cid";s:1:"3";}i:2;a:2:{s:8:"catename";s:6:"水果";s:3:"cid";s:1:"4";}i:3;a:2:{s:8:"catename";s:6:"海产";s:3:"cid";s:1:"5";}i:4;a:2:{s:8:"catename";s:6:"化工";s:3:"cid";s:1:"6";}i:5;a:2:{s:8:"catename";s:6:"茶类";s:3:"cid";s:1:"7";}i:6;a:2:{s:8:"catename";s:6:"家畜";s:3:"cid";s:1:"8";}i:7;a:2:{s:8:"catename";s:9:"工艺品";s:3:"cid";s:1:"9";}i:8;a:2:{s:8:"catename";s:6:"其他";s:3:"cid";s:2:"10";}}s:3:"msg";s:0:"";}
+?>
